625f1d4d33d579233dc6a496de312de65a884a4e
mggil7 <miguel.gustavo.gil@gmail.com>
2023-09-09 15:45:58 -0300

    Second Commit

---
+++ src/concepts/concept-1/navbar/Navbar.tsx
@@ -0,0 +1,44 @@
+import "./styles.css";
+
+import miguel from "./miguel_gil.jpeg";
+import logo from "./logo.svg";
+import { Dispatch, SetStateAction } from "react";
+
+export const Navbar = ({
+  setIsOpen,
+}: {
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}) => (
+  <nav className="navbar">
+    <button
+      onClick={() => setIsOpen(true)}
+      className="burger material-symbols-outlined"
+    >
+      menu
+    </button>
+    <div className="logo">
+      <img src={logo} alt ="logo"/>
+    </div>
+    <div className="center">
+      <div className="search">
+        <input
+          type="text"
+          className="search"
+          id="search"
+          placeholder="Search"
+        />
+        <button className="material-symbols-outlined">search</button>
+      </div>
+      <button className="material-symbols-outlined">mic</button>
+    </div>
+    <nav>
+      <button className="material-symbols-outlined">search</button>
+      <button className="material-symbols-outlined">mic</button>
+      <button className="material-symbols-outlined">video_call</button>
+      <button className="material-symbols-outlined">
+        <span className="badge">9+</span>notifications
+      </button>
+      <img src={miguel} alt=""/>
+    </nav>
+  </nav>
+);
